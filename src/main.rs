7884de72075479d14b41ccf02fbccb9ad073ccd5
Léo LEESCO <leo.leesco@gmail.com>
2025-01-10 12:08:19 +0100

    1 2
    print as state as hexa string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+fn state_to_string(state: [u64; 25]) -> String {
+    state.iter().fold("".to_string(), |acc, word| {
+        format!("{acc}{word:X}").to_string()
+    })
+}
+
 fn main() {
+    let state: [u64; 25];
+    let b: usize = 1600; // 1600 = 64 x 5 x 5
+    let r: usize;
+    let c: usize;
     println!("Hello, world!");
 }
