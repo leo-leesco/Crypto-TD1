4987f7f4f537096ced0f5062fa85354d1fab9909
Léo LEESCO <leo.leesco@gmail.com>
2025-01-18 18:32:08 +0100

    packing u8 to u64

--- src/convert.rs
+++ src/convert.rs
@@ -6,8 +6,18 @@ pub fn unpack_bytes(packed_bytes: u64) -> [u8; 8] {
         byte_array[i] = reducing as u8;
         reducing >>= 8 * size_of::<u8>(); // since size_of returns the size in bytes, you have to
                                           // count the number of bits per byte
+        #[cfg(test)]
         eprint!("{reducing:02X}");
+        #[cfg(test)]
         eprintln!(", extracted = {:02X}", byte_array[i]);
     }
     byte_array
+}
+
+pub fn pack_bytes(unpacked_bytes: [u8; 8]) -> u64 {
+    unpacked_bytes.iter().rev().fold(0, |packed, byt| {
+        #[cfg(test)]
+        eprint!("{packed:02X}");
+        (packed << (8 * size_of::<u8>())) + (*byt as u64)
+    })
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,19 +1,22 @@
-use TD1::convert::unpack_bytes;
+use TD1::convert::*;
+use TD1::print::*;
 
 #[test]
 fn print_zero() {
     let state: [u64; 25] = [0; 25];
-    let out = TD1::print::state_to_string(state);
+    let out = state_to_string(state);
     eprintln!("{out}, length = {}", out.len());
     assert_eq!(out.len(), 25 * 8);
 }
 
+const UNPACKED: [u8; 8] = [0x00, 0x0C, 0x11, 0xAA, 0x0A, 0xFF, 0x00, 0x10];
+const PACKED: u64 = 0x10_00_FF_0A_AA_11_0C_00;
+
+#[test]
+fn unpacking() {
+    assert_eq!(unpack_bytes(PACKED), UNPACKED);
+}
 #[test]
 fn packing() {
-    let bytes = [0x00, 0x0C, 0x11, 0xAA, 0x0A, 0xFF, 0x00, 0x10];
-    let packed = 0x10_00_FF_0A_AA_11_0C_00;
-    eprintln!("{:X}", packed);
-    eprintln!("Unpacking…");
-    let unpacked = unpack_bytes(packed);
-    assert_eq!(unpacked, bytes);
+    assert_eq!(PACKED, pack_bytes(UNPACKED));
 }
